2a57df2e1d04d60f93d6bae7fb313887f9db6802
tjallen27 <tjallen91@hotmail.co.uk>
2017-10-17 11:22:32 +0100

    Added API constant and filter func

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,8 @@
 angular
-  .module('app', [])
-  .controller('MainCtrl', MainCtrl);
-
-
-// $inject prevents errors when using Gulp
-MainCtrl.$inject = ['$http'];
-function MainCtrl($http){
-  // the 'vm' namespacing refers to the view model
-  const vm = this;
-  vm.all = [];
-
-  postsIndex();
-
-  function postsIndex(){
-    // http get data from this end point
-    $http.get('APIENDPOINT')
-    // after data has been retreived, take the response and...
-    .then((response) => {
-      // console log the response
-      console.log(response);
-    });
-  }
-}
+  .module('app', ['ui.router', 'ng-resource', 'satelizer', 'checklist-model'])
+  .constant('API_URL', 'http://localhost:3000')
+  .filter('startFrom', function(){
+    return function(data, start){
+      return data.slice(start);
+    };
+  });
