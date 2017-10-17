e564626bca0b5a4930a214f3022d222f8a17311d
tjallen27 <tjallen91@hotmail.co.uk>
2017-10-17 11:39:31 +0100

    Check error messages on rootScope

--- src/js/controllers/main.js
+++ src/js/controllers/main.js
@@ -7,9 +7,23 @@ MainCtrl.$inject = ['$rootScope', '$state', '$auth'];
 funtion MainCtrl($rootScope, $state, $auth){
   const vm = this;
 
+ // $rootScope is a parent object of all $scope Angular objects created in a web page
   $rootScope.$on('$stateChangeSuccess', () => {
     if(vm.stateHasChanged) vm.message = null;
     if (!vm.stateHasChanged) vm.stateHasChanged = true;
     vm.isNavCollapsed = true;
-  })
+  });;
+
+  vm.isAuthenticated = $auth.isAuthenticated;
+
+  // If there are any errors...
+  $rootScope.$on('error', (e, err)=>{
+    // ...change to the state to false
+    vm.stateHasChanged = false;
+    // store error message
+    vm.message = err.data.message;
+    // redirect to 'login' page
+    $state.go('login');
+  });
+
 }
