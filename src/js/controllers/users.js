0afd32519c01725e1b095d7822e4e3c8704bb8d3
tjallen27 <tjallen91@hotmail.co.uk>
2017-10-17 12:36:13 +0100

    Users controller added

---
+++ src/js/controllers/users.js
@@ -0,0 +1,27 @@
+angular
+  .module('app')
+  .controller('UsersIndexCtrl', UsersIndexCtrl)
+  .controller('UsersShowCtrl', UsersShowCtrl);
+
+UsersIndexCtrl.$inject = ['User'];
+function UsersIndexCtrl(User){
+  const vm = this;
+
+  vm.all = User.query();
+}
+
+UsersShowCtrl.$inject = ['User', '$stateParams', '$state', '$auth'];
+function UsersShowCtrl(User, $stateParams, $state, $auth){
+  const vm = this;
+  vm.user = User.get($stateParams);
+
+  function usersDelete(){
+    vm.user
+      .remove()
+      .then(()=> {
+        $auth.logout();
+        $state.go('register');
+      });
+  }
+  vm.delete = usersDelete;
+}
