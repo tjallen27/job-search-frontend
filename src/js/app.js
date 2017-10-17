3dc719e119fab39aff70cbc492916fabb9065353
tjallen27 <tjallen91@hotmail.co.uk>
2017-10-17 10:48:05 +0100

    http postIndex function created

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,24 @@
 angular
-  .module('app', []);
+  .module('app', [])
+  .controller('MainCtrl', MainCtrl);
+
+
+// $inject prevents errors when using Gulp
+MainCtrl.$inject = ['$http'];
+function MainCtrl($http){
+  // the 'vm' namespacing refers to the view model
+  const vm = this;
+  vm.all = [];
+
+  postsIndex();
+
+  function postsIndex(){
+    // http get data from this end point
+    $http.get('APIENDPOINT')
+    // after data has been retreived, take the response and...
+    .then((response) => {
+      // console log the response
+      console.log(response);
+    });
+  }
+}
