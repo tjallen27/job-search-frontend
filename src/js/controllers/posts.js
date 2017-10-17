cecd3fd08829e7a55f131115749b7a5369b46b2b
tjallen27 <tjallen91@hotmail.co.uk>
2017-10-17 12:02:01 +0100

    Post create function created

--- src/js/controllers/posts.js
+++ src/js/controllers/posts.js
@@ -5,8 +5,24 @@ angular
   .controller('PostsShowCtrl', PostsShowCtrl)
   .controller('PostsEditCtrl', PostsEditCtrl);
 
+// This controller provides all the data for the index page
 PostsIndexCtrl.$inject = ['Post', 'filterFilter', '$scope'];
-function PostsIndexCtrl(post, filterFilter, $scope){
+function PostsIndexCtrl(Post, filterFilter, $scope){
   const vm = this;
   vm.all = Post.query();
+}
+
+PostsNewCtrl.$inject = ['Post', 'User', '$state'];
+function PostsNewCtrl(Post, User, $state){
+  const vm = this;
+  vm.post = {};
+  vm.user = User.query();
+
+  function postsCreate(){
+    Post
+      .save({post: vm.post})
+      .$promise
+      .then(()=> $state.go('postsIndex'));
+  }
+  vm.create = postsCreate;
 }
