38fb07fb140c4b6ecef63ddb454438cb9c4c2521
tjallen27 <tjallen91@hotmail.co.uk>
2017-10-17 13:51:54 +0100

    base href added to index.html

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,3 @@
 angular
-  .module('app', ['ui.router', 'ng-resource', 'satellizer', 'checklist-model', 'ui.bootstrap', 'ngAnimate', 'ngMessages'])
-  .constant('API_URL', 'http://localhost:3000')
-  .filter('startFrom', function(){
-    return function(data, start){
-      return data.slice(start);
-    };
-  });
+  .module('app', ['ui.router', 'ngResource', 'satellizer', 'checklist-model', 'ui.bootstrap', 'ngAnimate', 'ngMessages'])
+  .constant('API_URL', 'http://localhost:3000');
